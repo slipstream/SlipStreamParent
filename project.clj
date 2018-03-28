715405cf39dc9c917203b27224d225f07890a944
khaled basbous <khaled@sixsq.com>
2018-03-28 12:24:56 +0200

    update dependencies to last version

--- project.clj
+++ project.clj
@@ -47,7 +47,7 @@
    [buddy/buddy-sign "2.2.0"]
 
    [camel-snake-kebab "0.4.0"]
-   [cc.qbits/spandex "0.6.1"]
+   [cc.qbits/spandex "0.6.2"]
    [cheshire "5.8.0"]                                       ;; newer, explicit version needed by ring-json
    [clj-http "3.8.0"]
    [clj-stacktrace "0.2.8"]
@@ -57,20 +57,20 @@
    [cljsjs/semantic-ui-react "0.78.2-0" :exclusions [cljsjs/react]]
    [cljsjs/moment "2.17.1-1"]
    [cljsjs/react-date-range "0.2.4-0" :exclusions [cljsjs/react]]
-   [cljsjs/vega-embed "3.0.0-rc7-0"]
+   [cljsjs/vega-embed "3.1.1-0"]
    [commons-logging "1.2"]
    [commons-lang/commons-lang "2.6"]
    [commons-codec/commons-codec "1.11"]
    [compojure "1.6.0"]
-   [com.amazonaws/aws-java-sdk-s3 "1.11.292"]
+   [com.amazonaws/aws-java-sdk-s3 "1.11.303"]
    [com.andrewmcveigh/cljs-time "0.5.2"]
    [com.cemerick/url "0.1.1"
     :exclusions [com.cemerick/clojurescript.test]]
    [com.draines/postal "2.0.2"]
    [com.jcraft/jsch "0.1.54"]
    [com.rpl/specter "1.1.0"]
-   [com.taoensso/encore "2.93.0"]
-   [com.taoensso/tempura "1.1.2"]
+   [com.taoensso/encore "2.94.0"]
+   [com.taoensso/tempura "1.2.0"]
    [com.taoensso/tower "3.1.0-beta5"]
    [org.clojure/tools.reader "1.1.0"]
 
@@ -79,7 +79,7 @@
    [com.taoensso/timbre "4.7.4"]
 
    ;; cljs testing; control options here
-   [doo "0.1.8" :scope "test"]
+   [doo "0.1.10" :scope "test"]
 
    [duratom "0.3.7"]
 
@@ -96,16 +96,16 @@
     :exclusions [org.clojure/clojurescript]]
 
    [javax.mail/mail "1.4.7" :scope "compile"]
-   [javax.servlet/javax.servlet-api "3.1.0"]
+   [javax.servlet/javax.servlet-api "4.0.0"]
 
    [log4j "1.2.17"
     :exclusions [javax.mail/mail
                  javax.jms/jms
                  com.sun.jdmk/jmxtools
                  com.sun.jmx/jmxri]]
-   [org.apache.logging.log4j/log4j-core "2.10.0"]
-   [org.apache.logging.log4j/log4j-api "2.10.0"]
-   [org.apache.logging.log4j/log4j-web "2.10.0"]
+   [org.apache.logging.log4j/log4j-core "2.11.0"]
+   [org.apache.logging.log4j/log4j-api "2.11.0"]
+   [org.apache.logging.log4j/log4j-web "2.11.0"]
    [org.slf4j/slf4j-simple "1.7.25"]
 
    [me.raynes/fs "1.4.6"]
@@ -163,7 +163,7 @@
    [clojure-complete "0.2.5" :scope "test"
     :exclusions [org.clojure/clojure]]
 
-   [day8.re-frame/re-frame-10x "0.2.0" :scope "test"]
+   [day8.re-frame/re-frame-10x "0.3.0" :scope "test"]
 
    [expectations "2.1.9" :scope "test"]
 
